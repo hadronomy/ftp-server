928f1cb5815450c2e6449d612912d8cb32a04e19
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-05-05 13:09:35 +0100

    feat: mark `FEAT` command as unimplemented

--- src/ftp/command/feat.rs
+++ src/ftp/command/feat.rs
@@ -17,13 +17,7 @@ impl<'a> FTPCommand<'a> for Feat {
         _writer: &mut tokio::net::tcp::WriteHalf<'b>,
     ) -> Result<Option<StatusCode>> {
         trace!("Reporting supported features");
-        Ok(Some(StatusCode::SystemStatus(
-            r"
-Features:
-211 END
-            "
-            .to_string(),
-        )))
+        Ok(Some(StatusCode::CmdNotImplemented))
     }
 }
 
