8adcf31cd1edb2e6da251b8b439979c0fa91cf74
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-05-08 19:45:01 +0100

    feat: allow empty password

--- src/ftp/command/pass.rs
+++ src/ftp/command/pass.rs
@@ -22,8 +22,9 @@ impl<'a> TryFrom<(&'a str, Vec<&'a str>)> for Pass<'a> {
 
     fn try_from((command, args): (&'a str, Vec<&'a str>)) -> Result<Self> {
         if command == Self::KEYWORD {
-            if args.len() == 1 {
-                Ok(Self(args[0]))
+            if (0..=1).contains(&args.len()) {
+                let password = args.first().copied().unwrap_or("anonymous");
+                Ok(Self(password))
             } else {
                 Err(miette!("Invalid number of arguments"))
             }
